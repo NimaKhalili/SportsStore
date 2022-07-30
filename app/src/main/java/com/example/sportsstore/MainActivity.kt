a5b2d271c97f9c1bf2ddd79ccb9a40ffcabfa44d
Nima <khalili7091@gmail.com>
2022-07-30 16:48:54 +0430

    Update mainActivity replace extends

--- app/src/main/java/com/example/sportsstore/MainActivity.kt
+++ app/src/main/java/com/example/sportsstore/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.sportsstore
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : SportsActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
