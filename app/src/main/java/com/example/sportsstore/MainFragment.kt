e5b75c36e0d963a37ae17669868390e7348f5b8a
Nima <khalili7091@gmail.com>
2022-08-06 15:59:49 +0430

    Add mainActivity fragment

--- app/src/main/java/com/example/sportsstore/MainFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.example.sportsstore
-
-import com.example.sportsstore.common.SportsFragment
-
-class MainFragment: SportsFragment() {
-}
---
+++ app/src/main/java/com/example/sportsstore/feature/main/MainFragment.kt
@@ -0,0 +1,30 @@
+package com.example.sportsstore.feature.main
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.sportsstore.R
+import com.example.sportsstore.common.SportsFragment
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import timber.log.Timber
+
+class MainFragment : SportsFragment() {
+    val mainViewModel: MainViewModel by viewModel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mainViewModel.productsLiveData.observe(this){
+            Timber.i(it.toString())
+        }
+    }
+}
