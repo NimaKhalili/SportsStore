924fabe369b12d578462a36ed2b87b582459ecd1
Nima <khalili7091@gmail.com>
2022-09-04 19:28:04 +0430

    Add cartRepository.kt

---
+++ app/src/main/java/com/example/sportsstore/data/repo/CartRepository.kt
@@ -0,0 +1,16 @@
+package com.example.sportsstore.data.repo
+
+import com.example.sportsstore.data.AddToCartResponse
+import com.example.sportsstore.data.CartItemCount
+import com.example.sportsstore.data.CartResponse
+import com.example.sportsstore.data.MessageResponse
+import io.reactivex.Single
+
+interface CartRepository {
+
+    fun addToCart(productId: Int): Single<AddToCartResponse>
+    fun get(): Single<CartResponse>
+    fun remove(cartItemId: Int): Single<MessageResponse>
+    fun changeCount(cartItemId: Int, count: Int): Single<AddToCartResponse>
+    fun getCartItemsCount(): Single<CartItemCount>
+}
