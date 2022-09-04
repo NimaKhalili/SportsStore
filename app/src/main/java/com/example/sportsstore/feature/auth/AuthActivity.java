d46a54aadc11a2a1db0753b74c01195e6242d8de
Nima <khalili7091@gmail.com>
2022-09-04 19:22:38 +0430

    Add authActivity

---
+++ app/src/main/java/com/example/sportsstore/feature/auth/AuthActivity.java
@@ -0,0 +1,16 @@
+package com.example.sportsstore.feature.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sportsstore.R;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
