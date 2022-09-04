d5fa74d71e9466bea84696fe398d7d0c0cde4c91
Nima <khalili7091@gmail.com>
2022-09-04 19:27:25 +0430

    Add cartRemoteDataSource.kt

---
+++ app/src/main/java/com/example/sportsstore/data/repo/source/CartRemoteDataSource.kt
@@ -0,0 +1,33 @@
+package com.example.sportsstore.data.repo.source
+
+import com.example.sportsstore.data.AddToCartResponse
+import com.example.sportsstore.data.CartItemCount
+import com.example.sportsstore.data.CartResponse
+import com.example.sportsstore.data.MessageResponse
+import com.example.sportsstore.services.http.ApiService
+import com.google.gson.JsonObject
+import io.reactivex.Single
+
+class CartRemoteDataSource(val apiService: ApiService):CartDataSource {
+    override fun addToCart(productId: Int): Single<AddToCartResponse> = apiService.addToCart(
+        JsonObject().apply {
+            addProperty("product_id", productId)
+        }
+    )
+
+    override fun get(): Single<CartResponse> {
+        TODO("Not yet implemented")
+    }
+
+    override fun remove(cartItemId: Int): Single<MessageResponse> {
+        TODO("Not yet implemented")
+    }
+
+    override fun changeCount(cartItemId: Int, count: Int): Single<AddToCartResponse> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCartItemsCount(): Single<CartItemCount> {
+        TODO("Not yet implemented")
+    }
+}
