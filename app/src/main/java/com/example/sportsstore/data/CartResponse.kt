987cb361806f8cd8f9c8bd0183113d498ce8f257
Nima <khalili7091@gmail.com>
2022-09-04 19:24:59 +0430

    Add cartResponse.kt

---
+++ app/src/main/java/com/example/sportsstore/data/CartResponse.kt
@@ -0,0 +1,9 @@
+package com.example.sportsstore.data
+
+
+data class CartResponse(
+    val cart_items: List<CartItem>,
+    val payable_price: Int,
+    val shipping_cost: Int,
+    val total_price: Int
+)
