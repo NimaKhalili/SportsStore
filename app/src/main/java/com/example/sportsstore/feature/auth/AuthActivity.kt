6f9a445d9e6af1634f8645996ff3f245c3f582ec
Nima <khalili7091@gmail.com>
2022-09-05 17:37:49 +0430

    Update authActivity change to kotlin

---
+++ app/src/main/java/com/example/sportsstore/feature/auth/AuthActivity.kt
@@ -0,0 +1,16 @@
+package com.example.sportsstore.feature.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sportsstore.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainer, LoginFragment()).commit()
+        }
+    }
+}
