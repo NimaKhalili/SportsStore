704aea152fc0edaa2c2e0bfd249097fb10560a2b
Nima <khalili7091@gmail.com>
2022-09-19 09:38:46 +0430

    Add loginFragment login method and replace signUpFragment

--- app/src/main/java/com/example/sportsstore/feature/auth/LoginFragment.kt
+++ app/src/main/java/com/example/sportsstore/feature/auth/LoginFragment.kt
@@ -5,14 +5,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.sportsstore.R
+import com.example.sportsstore.common.SportsCompletableObserver
+import io.reactivex.Completable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.fragment_login.emailEt
+import kotlinx.android.synthetic.main.fragment_login.passwordEt
+import kotlinx.android.synthetic.main.fragment_sign_up.*
 
 class LoginFragment: Fragment() {
+    val viewModel:AuthViewModel by viewModels()
+    val compositeDisposable = CompositeDisposable()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loginBtn.setOnClickListener{
+            viewModel.login(emailEt.text.toString(), passwordEt.text.toString())
+                .subscribeOn(Schedulers.io())
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : SportsCompletableObserver(compositeDisposable){
+                    override fun onComplete() {
+                        requireActivity().finish()
+                    }
+                })
+        }
+
+        signUpLinkBtn.setOnClickListener{
+            requireActivity().supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentContainer, SingUpFragment())
+            }.commit()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        compositeDisposable.clear()
     }
 }
