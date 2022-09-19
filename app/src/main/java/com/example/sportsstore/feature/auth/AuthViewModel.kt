a099bafeb29d6c12760bfc93f01186c453778646
Nima <khalili7091@gmail.com>
2022-09-19 09:35:48 +0430

    Add authViewModel.kt

---
+++ app/src/main/java/com/example/sportsstore/feature/auth/AuthViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.sportsstore.feature.auth
+
+import com.example.sportsstore.common.SportsViewModel
+import com.example.sportsstore.data.repo.UserRepository
+import io.reactivex.Completable
+
+class AuthViewModel(private val userRepository: UserRepository):SportsViewModel() {
+
+    fun login(email: String, password: String): Completable{
+        progressBarLiveData.value = true
+        return userRepository.login(email, password).doFinally {
+            progressBarLiveData.value = false
+        }
+    }
+
+    fun signUp(email: String, password: String): Completable{
+        progressBarLiveData.value = true
+        return userRepository.signUp(email, password).doFinally {
+            progressBarLiveData.postValue(false)
+        }
+    }
+
+}
