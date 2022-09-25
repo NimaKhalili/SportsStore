f7dc86530800d18763238b656c5ec71f27a14989
Nima <khalili7091@gmail.com>
2022-09-25 16:46:41 +0330

    Add apiService.kt new requests

--- app/src/main/java/com/example/sportsstore/services/http/ApiService.kt
+++ app/src/main/java/com/example/sportsstore/services/http/ApiService.kt
@@ -17,28 +17,40 @@ import retrofit2.http.Query
 
 interface ApiService {
     @GET("product/list")
-    fun getProducts(@Query("sort") sort:String):Single<List<Product>>
+    fun getProducts(@Query("sort") sort: String): Single<List<Product>>
 
     @GET("banner/slider")
-    fun getBanners():Single<List<Banner>>
+    fun getBanners(): Single<List<Banner>>
 
     @GET("comment/list")
     fun getComments(@Query("product_id") productId: Int): Single<List<Comment>>
 
     @POST("cart/add")
-    fun addToCart(@Body jsonObject: JsonObject):Single<AddToCartResponse>
+    fun addToCart(@Body jsonObject: JsonObject): Single<AddToCartResponse>
+
+    @POST("cart/remove")
+    fun removeItemFromCart(@Body jsonObject: JsonObject): Single<MessageResponse>
+
+    @GET("cart/list")
+    fun getCart(): Single<CartResponse>
+
+    @POST("cart/changeCount")
+    fun changeCount(@Body jsonObject: JsonObject): Single<AddToCartResponse>
+
+    @GET("cart/count")
+    fun getCartItemsCount(): Single<CartItemCount>
 
     @POST("auth/token")
-    fun login(@Body jsonObject: JsonObject):Single<TokenResponse>
+    fun login(@Body jsonObject: JsonObject): Single<TokenResponse>
 
     @POST("user/register")
-    fun signUp(@Body jsonObject: JsonObject):Single<MessageResponse>
+    fun signUp(@Body jsonObject: JsonObject): Single<MessageResponse>
 
     @POST("auth/token")
-    fun refreshToken(@Body jsonObject: JsonObject):Call<TokenResponse>
+    fun refreshToken(@Body jsonObject: JsonObject): Call<TokenResponse>
 }
 
-fun createApiServiceInstance ():ApiService{
+fun createApiServiceInstance(): ApiService {
 
     val okHttpClient = OkHttpClient.Builder()
         .addInterceptor {
@@ -56,7 +68,7 @@ fun createApiServiceInstance ():ApiService{
         })
         .build()
 
-    val retrofit=Retrofit.Builder()
+    val retrofit = Retrofit.Builder()
         .baseUrl("http://expertdevelopers.ir/api/v1/")
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .addConverterFactory(GsonConverterFactory.create())
