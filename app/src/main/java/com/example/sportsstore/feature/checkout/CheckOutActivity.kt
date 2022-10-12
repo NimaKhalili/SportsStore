6dca86f53eb2108856855c8c1a035e5989d16647
NimaKhalili <khalili7091@gmail.com>
2022-10-12 07:51:46 +0330

    Add checkOutActivity

---
+++ app/src/main/java/com/example/sportsstore/feature/checkout/CheckOutActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sportsstore.feature.checkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sportsstore.R
+
+class CheckOutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_out)
+    }
+}
