915a1a26628546678ceb8a65599cb62511381534
NimaKhalili <khalili7091@gmail.com>
2022-10-12 08:18:35 +0330

    Add shippingActivity viewModel and get purchaseDetail and put in UI with using purchaseDetailViewHolder method from cartItemAdapter

--- app/src/main/java/com/example/sportsstore/feature/shipping/ShippingActivity.kt
+++ app/src/main/java/com/example/sportsstore/feature/shipping/ShippingActivity.kt
@@ -3,10 +3,20 @@ package com.example.sportsstore.feature.shipping
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.sportsstore.R
+import com.example.sportsstore.common.EXTRA_KEY_DATA
+import com.example.sportsstore.data.PurchaseDetail
+import com.example.sportsstore.feature.cart.CartItemAdapter
+import kotlinx.android.synthetic.main.activity_shipping.*
 
 class ShippingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shipping)
+
+        val purchaseDetail = intent.getParcelableExtra<PurchaseDetail>(EXTRA_KEY_DATA)
+            ?: throw IllegalStateException("purchase detail cannot be null")
+
+        val viewHolder = CartItemAdapter.PurchaseDetailViewHolder(purchaseDetailView)
+        viewHolder.bind(purchaseDetail.totalPrice, purchaseDetail.shipping_cost,purchaseDetail.payable_price)
     }
 }
