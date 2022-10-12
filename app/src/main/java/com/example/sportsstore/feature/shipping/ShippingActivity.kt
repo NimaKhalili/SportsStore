5c012f84ab01d18eac41c882de8ee4f0865b92cc
NimaKhalili <khalili7091@gmail.com>
2022-10-12 07:52:30 +0330

    Add shippingActivity

---
+++ app/src/main/java/com/example/sportsstore/feature/shipping/ShippingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sportsstore.feature.shipping
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sportsstore.R
+
+class ShippingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shipping)
+    }
+}
