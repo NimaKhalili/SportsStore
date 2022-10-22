ea565cdd2b9ddad61941016945999d8d76fc389b
NimaKhalili <khalili7091@gmail.com>
2022-10-22 19:38:18 +0330

    Add shippingActivity.kt viewModel

--- app/src/main/java/com/example/sportsstore/feature/shipping/ShippingActivity.kt
+++ app/src/main/java/com/example/sportsstore/feature/shipping/ShippingActivity.kt
@@ -1,14 +1,27 @@
 package com.example.sportsstore.feature.shipping
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.sportsstore.R
 import com.example.sportsstore.common.EXTRA_KEY_DATA
+import com.example.sportsstore.common.EXTRA_KEY_ID
+import com.example.sportsstore.common.SportsSingleObserver
+import com.example.sportsstore.common.openUrlInCustomTab
 import com.example.sportsstore.data.PurchaseDetail
+import com.example.sportsstore.data.SubmitOrderResult
 import com.example.sportsstore.feature.cart.CartItemAdapter
+import com.example.sportsstore.feature.checkout.CheckOutActivity
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_shipping.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ShippingActivity : AppCompatActivity() {
+    val viewModel: ShippingViewModel by viewModel()
+    val compositeDisposable = CompositeDisposable()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shipping)
@@ -17,6 +30,42 @@ class ShippingActivity : AppCompatActivity() {
             ?: throw IllegalStateException("purchase detail cannot be null")
 
         val viewHolder = CartItemAdapter.PurchaseDetailViewHolder(purchaseDetailView)
-        viewHolder.bind(purchaseDetail.totalPrice, purchaseDetail.shipping_cost,purchaseDetail.payable_price)
+        viewHolder.bind(
+            purchaseDetail.totalPrice,
+            purchaseDetail.shipping_cost,
+            purchaseDetail.payable_price
+        )
+        val onClickListener = View.OnClickListener {
+            viewModel.submitOrder(
+                firstNameEt.text.toString(),
+                lastNameEt.text.toString(),
+                postalCodeEt.text.toString(),
+                phoneNumberEt.text.toString(),
+                addressEt.text.toString(),
+                if (it.id == R.id.onlinePaymentBtn) PAYMENT_METHOD_ONLINE else PAYMENT_METHOD_COD
+            ).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : SportsSingleObserver<SubmitOrderResult>(compositeDisposable) {
+                    override fun onSuccess(t: SubmitOrderResult) {
+                        if (t.bank_gateway_url.isNotEmpty()) {
+                            openUrlInCustomTab(this@ShippingActivity, t.bank_gateway_url)
+                        } else {
+                            startActivity(
+                                Intent(
+                                    this@ShippingActivity,
+                                    CheckOutActivity::class.java
+                                ).apply {
+                                    putExtra(EXTRA_KEY_ID, t.order_id)
+                                }
+                            )
+                        }
+
+                        finish()
+                    }
+                })
+        }
+
+        onlinePaymentBtn.setOnClickListener(onClickListener)
+        codBtn.setOnClickListener(onClickListener)
     }
 }
