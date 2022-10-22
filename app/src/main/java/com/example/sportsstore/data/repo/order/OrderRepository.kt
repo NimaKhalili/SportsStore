19a753386f79fd6af17b41c9c55d09570c50a51c
NimaKhalili <khalili7091@gmail.com>
2022-10-22 19:33:15 +0330

    Add orderRepository.kt

---
+++ app/src/main/java/com/example/sportsstore/data/repo/order/OrderRepository.kt
@@ -0,0 +1,19 @@
+package com.example.sportsstore.data.repo.order
+
+import com.example.sportsstore.data.Checkout
+import com.example.sportsstore.data.SubmitOrderResult
+import io.reactivex.Single
+
+interface OrderRepository {
+
+    fun submit(
+        firstName: String,
+        lastName: String,
+        postalCode: String,
+        phoneNumber: String,
+        address: String,
+        paymentMethod: String
+    ): Single<SubmitOrderResult>
+
+    fun checkOut(orderId: Int): Single<Checkout>
+}
