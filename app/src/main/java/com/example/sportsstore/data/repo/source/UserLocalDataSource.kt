d45e39cec5309e61ecc7a16bde0c78ce2ddb3f79
NimaKhalili <khalili7091@gmail.com>
2022-10-23 17:04:24 +0330

    Add userLocalDataSource.kt profile methods

--- app/src/main/java/com/example/sportsstore/data/repo/source/UserLocalDataSource.kt
+++ app/src/main/java/com/example/sportsstore/data/repo/source/UserLocalDataSource.kt
@@ -28,4 +28,15 @@ class UserLocalDataSource(val sharedPreferences: SharedPreferences) : UserDataSo
             putString("refresh_token", refreshToken)
         }.apply()
     }
+
+    override fun saveUserName(username: String) {
+            sharedPreferences.edit().apply{
+                putString("username", username)
+            }.apply()
+    }
+
+    override fun getUserName(): String = sharedPreferences.getString("username", "") ?: ""
+    override fun signOut() = sharedPreferences.edit().apply {
+        clear()
+    }.apply()
 }
